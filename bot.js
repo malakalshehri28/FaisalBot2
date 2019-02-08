8ef3b5e7e369903d0454a6b067acbd09b65240d7
malakalshehri28 <46494268+malakalshehri28@users.noreply.github.com>
2019-02-08 19:51:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+var prefix = "#";
 client.on('ready', () => {
    console.log(`----------------`);
       console.log(`Desert Bot- Script By : AJ`);
