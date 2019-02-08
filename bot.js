4dea3660cf171a6ed0bd5ef9a41436e1d443f728
malakalshehri28 <46494268+malakalshehri28@users.noreply.github.com>
2019-02-08 20:23:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,6 +15,101 @@ client.user.setGame(`Free Mic.`,"http://twitch.tv//idk")
 
 
 
+
+
+let data = JSON.parse(fs.readFileSync('./database.json' , 'utf8'));
+ 
+client.on('message', message => {
+    if(message.content === `${prefix}setAntifake`) {
+     let args = message.content.split(" ").slice(1).join(" ")
+if(!args) return message.channel.send(`Please To Set The AntiFake Write The Invite User Limt Example: ${prefix}setAntifake 7`)
+let embed = new Discord.RichEmbed()
+.setTitle('Done Antifake Setup Has Been Completed !')
+.addField('The Limt:', `${args}`)
+.addField('The Setup Started By', `${message.author}`)
+.setFooter(`Requested by : ${message.author}`)
+data[message.guild.id] = {
+limt: args,
+onoff: 'On'
+}
+fs.writeFile("./database.json", JSON.stringify(data), (err) => {
+    if (err) console.error(err)
+    .catch(err => {
+      console.error(err);
+    })
+})
+    }
+ 
+        if(message.content.startsWith(prefix + "toggleantifake")) {
+            if(!message.channel.guild) return message.reply('**This Command Only For Servers**');
+            if(!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('**Sorry But You Dont Have Permission** `MANAGE_GUILD`' );
+            if(!data[message.guild.id]) data[message.guild.id] = {
+                onoff: 'Off',
+            }
+              if(data[message.guild.id].onoff === 'Off') return [message.channel.send(`**The AntiFake Is __𝐎𝐍__ !**`), data[message.guild.id].onoff = 'On']
+              if(data[message.guild.id].onoff === 'On') return [message.channel.send(`**The Antifake Is __𝐎𝐅𝐅__ !**`), data[message.guild.id].onoff = 'Off']
+              fs.writeFile("./database.json", JSON.stringify(data), (err) => {
+                if (err) console.error(err)
+                .catch(err => {
+                  console.error(err);
+                })
+            })
+        }
+ 
+})
+ 
+ 
+ 
+var invs2 = {}
+var invites = {};
+ 
+client.on('ready', () => {
+    client.guilds.forEach(g => {
+        g.fetchInvites().then(guildInvites => {
+            invites[g.id] = guildInvites;
+        });
+    });
+});
+ 
+client.on('guildMemberAdd', member => {
+    if(!data[member.guild.id]) data[member.guild.id] = {
+        onoff: 'Off',
+      }
+      if(data[member.guild.id].onoff === 'Off') return;
+ 
+    member.guild.fetchInvites().then(ServerInvs => {
+        var ei = invites[member.guild.id];
+        invites[member.guild.id] = ServerInvs;
+        var invite = ServerInvs.find(i => ei.get(i.code).uses < i.uses);
+        if (!invs2[invite.code]) invs2[invite.code] = {users: new Set()};
+        invs2[invite.code].users.delete(member.id);
+        setTimeout(function(){
+            invs2[invite.code].users.remove(member.id);
+        },10000);
+        var x = 0;
+        invs2[invite.code].users.forEach(() => {
+            x++;
+            if (x >= data[member.guild.id].limt) {
+                invs2[invite.code].users.forEach(user => {
+                    member.guild.members.get(user).ban();
+                    fs.writeFile("./database.json", JSON.stringify(data), (err) => {
+                        if (err) console.error(err)
+                        .catch(err => {
+                          console.error(err);
+                        })
+                    })
+                });
+            };
+        });
+    });
+});
+
+
+
+
+
+
+
 var config = {
   events: [
     {type: "CHANNEL_CREATE", logType: "CHANNEL_CREATE", limit: 1 , delay: 3000},
