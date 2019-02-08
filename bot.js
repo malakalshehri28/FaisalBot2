f5a011aa0374f13e4746f34cc2de6bd3b07903bc
malakalshehri28 <46494268+malakalshehri28@users.noreply.github.com>
2019-02-08 18:57:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,11 @@
-
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : AJ`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : AJ ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Free Mic.`,"http://twitch.tv//idk")
+});
