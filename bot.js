8d8340f90dc540d041a6a387acbce768fa39dd61
malakalshehri28 <46494268+malakalshehri28@users.noreply.github.com>
2019-02-09 14:06:20 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "#";
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : AJ`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : AJ ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Bot System`,"http://twitch.tv//idk")
+});
+
+
+
+
+
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
